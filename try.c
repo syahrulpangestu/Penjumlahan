656cd5f104b30b5a0ac5795c681524f90afccfe0
syahrulpangestu_ <syahrulpangestu11@gmail.com>
2020-09-20 19:51:25 +0700

    commit kedua add "penjumlahan"

--- try.c
+++ try.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
+#include <conio.h>
+
 
 int main(){
     printf("komit pertama");
+    printf("komit kedua");
+    
+    int a = 1, b = 6, hasil;
+
+    hasil = a + b;
+    printf("Hasil penjumlahan dari 1 + 6 adalah %d",hasil);
 }
