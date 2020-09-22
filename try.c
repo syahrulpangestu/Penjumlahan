c0af0add8ad09a79ca7a938ec6b755473d39ff63
syahrulpangestu_ <syahrulpangestu11@gmail.com>
2020-09-22 20:42:23 +0700

    commit 3 add "cek lagi"

--- try.c
+++ try.c
@@ -5,9 +5,10 @@
 int main(){
     printf("komit pertama");
     printf("komit kedua");
+    printf("cek lagi");
     
     int a = 1, b = 6, hasil;
 
     hasil = a + b;
-    printf("Hasil penjumlahan dari 1 + 6 adalah %d",hasil);
+    printf("hasilnya adalah &d", hasil);
 }
